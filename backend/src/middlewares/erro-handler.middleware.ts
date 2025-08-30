b76cd9ab74d25099892ffc9f68967b837664a4fd
LucianoASF <lucianoaugustos12@gmail.com>
2025-08-29 21:11:03 -0300

    adicionando erro do servidor e erro de foreign key

--- backend/src/middlewares/erro-handler.middleware.ts
+++ backend/src/middlewares/erro-handler.middleware.ts
@@ -1,12 +1,21 @@
 import { errors } from 'celebrate';
 import type { Express, NextFunction, Request, Response } from 'express';
 import { ErrorBase } from '../errors/base.error.js';
+import { ForeignKeyConstraintError } from 'sequelize';
+import { InternalServerError } from '../errors/internal-server.error.js';
 
 export const erroHandler = (app: Express) => {
   app.use(errors());
   app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof ForeignKeyConstraintError) {
+      return res.status(400).json({
+        erro: 'Não foi possível salvar porque a referência fornecida não existe.',
+      });
+    }
     if (error instanceof ErrorBase) {
       error.send(res);
+    } else {
+      new InternalServerError().send(res);
     }
   });
 };
